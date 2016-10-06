621fb83e57398a11810f9057eec99c83b1c58314
Aaron Scherer <aequasi@gmail.com>
2016-10-06 09:39:15 -0700

    Adding static server

---
+++ server.js
@@ -0,0 +1,79 @@
+'use strict'
+const Hapi  = require('hapi')
+const Boom  = require('boom')
+const Good  = require('good')
+const Inert = require('inert')
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const server = new Hapi.Server()
+server.connection({
+    host:   '0.0.0.0',
+    port:   process.env.PORT || 8000,
+    routes: {
+        cors: true,
+    },
+})
+
+server.register([
+    {
+        register: Inert,
+    },
+    {
+        register: Good,
+        options:  {
+            ops:       {
+                interval: 5000,
+            },
+            reporters: {
+                console: [{
+                    module: 'good-squeeze',
+                    name:   'Squeeze',
+                    args:   [{log: '*', request: '*', response: '*', error: '*'}],
+                }, {
+                    module: 'good-console',
+                }, 'stdout'],
+            },
+        },
+    },
+], () => {
+    
+    server.route({
+        method:  'GET',
+        path:    '/static/{param*}',
+        handler: {
+            directory: {
+                path:             'static',
+                lookupCompressed: true,
+            },
+        },
+    })
+    
+    server.route({
+        method:  'GET',
+        path:    '/{param*}',
+        handler: {
+            directory: {
+                path:             'public',
+                lookupCompressed: true,
+            },
+        },
+    })
+    
+    server.ext('onPreResponse', (request, reply) => {
+        if (request.response.isBoom) {
+            if (request.response.output.statusCode === 404) {
+                return reply.redirect('/404')
+            }
+        }
+        
+        return reply.continue()
+        
+    })
+    
+    server.start(() => {
+        server.log(['info'], `Server running at: ${server.info.uri}`)
+    })
+})
